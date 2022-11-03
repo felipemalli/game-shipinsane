40632bfb3abd7b7de0802370605bd1f752165694
felipemalli <felipevahia@gmail.com>
2022-11-02 22:28:58 -0300

    feat: add rotation to first cannon

--- src/pages/shipinsane.py
+++ src/pages/shipinsane.py
@@ -1,3 +1,4 @@
+import pygame
 from PPlay.gameimage import *
 from PPlay.keyboard import *
 from PPlay.mouse import *
@@ -7,17 +8,14 @@ from PPlay.window import *
 
 def init():
 
-    #Configura o tamanho da janela
-    width = 1920
-    height = 1080
+    WIDTH = 1920
+    HEIGHT = 1080
 
-    window = Window(width, height)
+    window = Window(WIDTH, HEIGHT)
 
     window.set_title("ShipInsane")
 
     window.set_background_color([0, 0, 0])
-
-
 
     #Carrega os sprites de cada objeto do jogo
     island = Sprite("../assets/island2.png", 1)
@@ -32,17 +30,18 @@ def init():
     cannon3 = Sprite("../assets/cannon3.png")
     cannon4 = Sprite("../assets/cannon4.png")
 
+    keyboard = Window.get_keyboard()
 
     #Configura as posiçoes dos elementos do jogo
 
-    island.x = width / 2 - island.width / 2
-    island.y = height / 2 - island.height / 2
+    island.x = WIDTH / 2 - island.width / 2
+    island.y = HEIGHT / 2 - island.height / 2
 
-    enemy_ship_1.x = 5 * width / 6
+    enemy_ship_1.x = 5 * WIDTH / 6
     enemy_ship_1.y = island.x / 2 - 40
 
     enemy_ship_2.x = island.x / 2 - 40
-    enemy_ship_2.y = 5 * height / 7 
+    enemy_ship_2.y = 5 * HEIGHT / 7 
 
     enemy_pirate_1.x = island.x + island.width / 2 
     enemy_pirate_1.y = island.y + 50
@@ -65,6 +64,17 @@ def init():
     cannon4.x = island.x
     cannon4.y = island.y + island.height / 2
 
+    img0 = cannon1.image
+    rect0 = cannon1.image.get_rect()
+    rect0.center=(cannon1.x + cannon1.width, cannon2.y - cannon1.height)
+
+    def rot_center(image, rect, angle):
+        """rotate an image while keeping its center"""
+        rot_image = pygame.transform.rotate(image, angle)
+        rot_rect = rot_image.get_rect(center=rect.center)
+        return rot_image,rot_rect
+
+    angle=0
     while(True):
         
         sea.draw()
@@ -73,15 +83,39 @@ def init():
         enemy_pirate_2.draw()
         enemy_ship_1.draw()
         enemy_ship_2.draw()
-        cannon1.draw()
         cannon2.draw()
         cannon3.draw()
         cannon4.draw()
         player.draw()
+
+        if keyboard.key_pressed("A"):
+            angle += 1
+        if keyboard.key_pressed("D"):
+            angle -= 1
+        img1=pygame.transform.rotate(img0, angle)
+        rect1=img1.get_rect()
+        rect1.center=rect0.center
+
+        window.get_screen().blit(img1, rect1)
+
+        # if keyboard.key_pressed("A") and test == 1:
+        #     test = 0
+        #     cannon1.image, cannon1.rect = rot_center(cannon1.image, cannon_1_rect, 15)      
+        # if keyboard.key_pressed("D") and test == 0:
+        #     test = 1
+        #     cannon1.image, cannon1.rect = rot_center(cannon1.image, cannon_1_rect, -15)
         
-        
-        
-        
-        
+
+
+        # window.get_screen().blit(cannon1.image, cannon1.rect)
+
+        # if keyboard.key_pressed("A") and test == 1:
+        #     test = 0
+        #     cannon1.image = pygame.transform.rotate(cannon1.image, 15)        
+        # if keyboard.key_pressed("D") and test == 0:
+        #     test = 1
+        #     cannon1.image = pygame.transform.rotate(cannon1.image, -15)
+
+        # cannon1.draw()
         window.update()
         window.set_background_color([0, 0, 0,])
