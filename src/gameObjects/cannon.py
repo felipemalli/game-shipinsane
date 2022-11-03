99436b4740f76c7f9f37154ead15cc1473bcf3dd
felipemalli <felipevahia@gmail.com>
2022-11-03 00:49:42 -0300

    feat: implement cannon class

---
+++ src/gameObjects/cannon.py
@@ -0,0 +1,39 @@
+import pygame
+from PPlay.sprite import Sprite
+
+
+class Cannon:
+    def __init__(self, sprite, x, y):
+        self.cannon_list = []
+
+        self.cannon_spt = sprite
+        self.cannon_spt.x = x
+        self.cannon_spt.y = y
+
+        self.image = self.cannon_spt.image
+        self.rect = self.cannon_spt.image.get_rect()
+        self.rect.center=(self.cannon_spt.x - (self.cannon_spt.width / 2), self.cannon_spt.y + (self.cannon_spt.height / 2))
+
+        self.angle = 0
+
+    def get_img_rect(self):
+        return self.image, self.rect
+
+    def move_angle_anticlockwise(self):
+        if self.angle < 60:
+            self.angle += 1
+
+    def move_angle_clockwise(self):
+        if self.angle > -60:
+            self.angle -= 1
+    
+    def rot_center(self):
+        rot_image = pygame.transform.rotate(self.image, self.angle)
+        rot_rect = rot_image.get_rect(center=self.rect.center)
+        return rot_image,rot_rect
+
+    def get_is_on_screen(self):
+        return self.is_on_screen
+
+    def move_clockwise(self):
+        self.angle -= 1
