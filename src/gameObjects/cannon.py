3502fe42797d67f6fa6000225587ccdcfd38fb9f
felipemalli <felipevahia@gmail.com>
2022-11-03 17:24:13 -0300

    feat: implement cannon class methods for handle cannon_balls

--- src/gameObjects/cannon.py
+++ src/gameObjects/cannon.py
@@ -1,10 +1,12 @@
 import pygame
 from PPlay.sprite import Sprite
 
+from .cannon_ball import Cannon_ball
+
 
 class Cannon:
     def __init__(self, sprite, x, y):
-        self.cannon_list = []
+        self.cannon_ball_list = []
 
         self.cannon_spt = sprite
         self.cannon_spt.x = x
@@ -16,6 +18,12 @@ class Cannon:
 
         self.angle = 0
 
+    def get_angle(self):
+        return self.angle
+
+    def get_rect(self):
+        return self.rect
+
     def get_img_rect(self):
         return self.image, self.rect
 
@@ -24,18 +32,26 @@ class Cannon:
         rot_rect = rot_image.get_rect(center=self.rect.center)
         return rot_image,rot_rect
 
-    def move_angle_anticlockwise(self):
-        if self.angle < 60:
-            self.angle += 1
-        return self.rot_center()
-
-    def move_angle_clockwise(self):
-        if self.angle > -60:
-            self.angle -= 1
-        return self.rot_center()
-
-    def get_is_on_screen(self):
-        return self.is_on_screen
-
     def move_clockwise(self):
-        self.angle -= 1
+        if self.angle > -60:
+            self.angle -= 0.35
+        return self.rot_center()
+
+    def move_anticlockwise(self):
+        if self.angle < 60:
+            self.angle += 0.35
+        return self.rot_center()
+
+    def shot(self):
+        shot_speed = 1
+        cannon_ball = Cannon_ball(shot_speed, self)
+        self.cannon_ball_list.append(cannon_ball)
+
+    def remove_cannon_ball(self, cannon_ball):
+        print(cannon_ball)
+        self.cannon_ball_list.remove(cannon_ball)
+
+    def render_shots(self):
+        for cannon_ball in self.cannon_ball_list:
+            cannon_ball.draw()
+            cannon_ball.move_with_angle()
