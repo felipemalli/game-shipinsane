849a9a47e06b6f7646c225b408409f7e1e6e7483
felipemalli <felipevahia@gmail.com>
2022-11-03 00:51:44 -0300

    feat: implement cannon creation with class objects and organize code

--- src/pages/shipinsane.py
+++ src/pages/shipinsane.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import pygame
 from PPlay.gameimage import *
 from PPlay.keyboard import *
@@ -5,16 +8,16 @@ from PPlay.mouse import *
 from PPlay.sprite import *
 from PPlay.window import *
 
+sys.path.insert(0, os.path.abspath("../"))
+from gameObjects.cannon import Cannon
+from gameObjects.cannon_ball import Cannon_ball
+
 
 def init():
-
     WIDTH = 1920
     HEIGHT = 1080
-
     window = Window(WIDTH, HEIGHT)
-
     window.set_title("ShipInsane")
-
     window.set_background_color([0, 0, 0])
 
     #Carrega os sprites de cada objeto do jogo
@@ -25,10 +28,7 @@ def init():
     player = Sprite("../assets/player.png", 1)
     enemy_ship_1 = Sprite("../assets/enemy_ship1.png")
     enemy_ship_2 = Sprite("../assets/enemy_ship2.png")
-    cannon1 = Sprite("../assets/cannon1.png")
-    cannon2 = Sprite("../assets/cannon2.png")
-    cannon3 = Sprite("../assets/cannon3.png")
-    cannon4 = Sprite("../assets/cannon4.png")
+    # cannon1 = Sprite("../assets/cannon1.png")
 
     keyboard = Window.get_keyboard()
 
@@ -37,85 +37,53 @@ def init():
     island.x = WIDTH / 2 - island.width / 2
     island.y = HEIGHT / 2 - island.height / 2
 
-    enemy_ship_1.x = 5 * WIDTH / 6
-    enemy_ship_1.y = island.x / 2 - 40
+    # enemy_ship_1.x = 5 * WIDTH / 6
+    # enemy_ship_1.y = island.x / 2 - 40
+    # enemy_ship_2.x = island.x / 2 - 40
+    # enemy_ship_2.y = 5 * HEIGHT / 7 
+    # enemy_pirate_1.x = island.x + island.width / 2 
+    # enemy_pirate_1.y = island.y + 50
+    # enemy_pirate_2.x = island.x + island.width / 2 - 70
+    # enemy_pirate_2.y = island.y + 80
+    # player.x = island.x + island.width / 2 + 120
+    # player.y = island.y + island.height / 2 - 60
 
-    enemy_ship_2.x = island.x / 2 - 40
-    enemy_ship_2.y = 5 * HEIGHT / 7 
+    # cannon1.x = island.x + island.width / 2 - 15
+    # cannon1.y = island.y + 10
 
-    enemy_pirate_1.x = island.x + island.width / 2 
-    enemy_pirate_1.y = island.y + 50
+    # cannon2.x = island.x + island.width / 2 + 130
+    # cannon2.y = island.y + island.height / 2 - 70
 
-    enemy_pirate_2.x = island.x + island.width / 2 - 70
-    enemy_pirate_2.y = island.y + 80
+    # cannon3.x = island.x + island.width / 2 - 50
+    # cannon3.y = island.y + island.height - 50  
 
-    player.x = island.x + island.width / 2 + 120
-    player.y = island.y + island.height / 2 - 60
+    # cannon4.x = island.x
+    # cannon4.y = island.y + island.height / 2
 
-    cannon1.x = island.x + island.width / 2 - 25
-    cannon1.y = island.y + 20
+    cannon_north_x = island.x + island.width / 2 - 15
+    cannon_north_y = island.y + 10
+    cannon_north_sprite = Sprite("../assets/cannon_north.png")
+    cannon_north = Cannon(cannon_north_sprite, cannon_north_x, cannon_north_y)
+    cannon_north_img, cannon_north_rect = cannon_north.get_img_rect()
 
-    cannon2.x = island.x + island.width / 2 + 130
-    cannon2.y = island.y + island.height / 2 - 70
-
-    cannon3.x = island.x + island.width / 2 - 50
-    cannon3.y = island.y + island.height - 50  
-
-    cannon4.x = island.x
-    cannon4.y = island.y + island.height / 2
-
-    img0 = cannon1.image
-    rect0 = cannon1.image.get_rect()
-    rect0.center=(cannon1.x + cannon1.width, cannon2.y - cannon1.height)
-
-    def rot_center(image, rect, angle):
-        """rotate an image while keeping its center"""
-        rot_image = pygame.transform.rotate(image, angle)
-        rot_rect = rot_image.get_rect(center=rect.center)
-        return rot_image,rot_rect
-
-    angle=0
     while(True):
-        
         sea.draw()
         island.draw()
-        enemy_pirate_1.draw()
-        enemy_pirate_2.draw()
-        enemy_ship_1.draw()
-        enemy_ship_2.draw()
-        cannon2.draw()
-        cannon3.draw()
-        cannon4.draw()
-        player.draw()
 
         if keyboard.key_pressed("A"):
-            angle += 1
+            cannon_north.move_angle_anticlockwise()
+            cannon_north_img, cannon_north_rect = cannon_north.rot_center()
+
         if keyboard.key_pressed("D"):
-            angle -= 1
-        img1=pygame.transform.rotate(img0, angle)
-        rect1=img1.get_rect()
-        rect1.center=rect0.center
+            cannon_north.move_angle_clockwise()
+            cannon_north_img, cannon_north_rect = cannon_north.rot_center()
 
-        window.get_screen().blit(img1, rect1)
-
-        # if keyboard.key_pressed("A") and test == 1:
-        #     test = 0
-        #     cannon1.image, cannon1.rect = rot_center(cannon1.image, cannon_1_rect, 15)      
-        # if keyboard.key_pressed("D") and test == 0:
-        #     test = 1
-        #     cannon1.image, cannon1.rect = rot_center(cannon1.image, cannon_1_rect, -15)
-        
-
-
-        # window.get_screen().blit(cannon1.image, cannon1.rect)
-
-        # if keyboard.key_pressed("A") and test == 1:
-        #     test = 0
-        #     cannon1.image = pygame.transform.rotate(cannon1.image, 15)        
-        # if keyboard.key_pressed("D") and test == 0:
-        #     test = 1
-        #     cannon1.image = pygame.transform.rotate(cannon1.image, -15)
-
-        # cannon1.draw()
+        window.get_screen().blit(cannon_north_img, cannon_north_rect)
+    
+        # enemy_pirate_1.draw()
+        # enemy_pirate_2.draw()
+        # enemy_ship_1.draw()
+        # enemy_ship_2.draw()
+        # player.draw()
         window.update()
         window.set_background_color([0, 0, 0,])
