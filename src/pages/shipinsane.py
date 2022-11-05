4fe7697313991f36f8199c531a692901198bcfa3
felipemalli <felipevahia@gmail.com>
2022-11-05 10:52:12 -0300

    feat: put all cannons inside the game

--- src/pages/shipinsane.py
+++ src/pages/shipinsane.py
@@ -27,7 +27,6 @@ def init():
     player = Sprite("../assets/player.png", 1)
     enemy_ship_1 = Sprite("../assets/enemy_ship1.png")
     enemy_ship_2 = Sprite("../assets/enemy_ship2.png")
-    cannon_ball = Sprite("../assets/cannon_ball.png")
 
     keyboard = Window.get_keyboard()
 
@@ -57,37 +56,63 @@ def init():
     # cannon4.x = island.x
     # cannon4.y = island.y + island.height / 2
 
-    cannon_north_x = island.x + island.width / 2 - 15
-    cannon_north_y = island.y + 20
-    cannon_north_sprite = Sprite("../assets/cannon_north.png")
-    cannon_north = Cannon(cannon_north_sprite, cannon_north_x, cannon_north_y)
+
+    # Cannons of island below ---------------------
+
+    cannon_north = Cannon(0, Sprite("../assets/cannon_north.png"), island.x + island.width / 2 - 15, island.y + 20)
     cannon_north_img, cannon_north_rect = cannon_north.get_img_rect()
+
+    cannon_south = Cannon(180, Sprite("../assets/cannon_south.png"), island.x + island.width / 2 - 50, island.y + island.height - 50)
+    cannon_south_img, cannon_south_rect = cannon_south.get_img_rect()
+
+    cannon_west = Cannon(90, Sprite("../assets/cannon_west.png"), island.x, island.y + island.height / 2)
+    cannon_west_img, cannon_west_rect = cannon_west.get_img_rect()
+
+    cannon_north_east = Cannon(300, Sprite("../assets/cannon_north_east.png"), island.x + island.width / 2 + 130, island.y + island.height / 2 - 70)
+    cannon_north_east_img, cannon_north_east_rect = cannon_north_east.get_img_rect()
+
+    # ---------------------------------------------
 
     cooldown_time = 0.5
     shot_cooldown = cooldown_time
+
     while(True):
         sea.draw()
         island.draw()
 
         if keyboard.key_pressed("A"):
             cannon_north_img, cannon_north_rect = cannon_north.move_anticlockwise()
+            cannon_south_img, cannon_south_rect = cannon_south.move_anticlockwise()
+            cannon_west_img, cannon_west_rect = cannon_west.move_anticlockwise()
+            cannon_north_east_img, cannon_north_east_rect = cannon_north_east.move_anticlockwise()
 
         if keyboard.key_pressed("D"):
             cannon_north_img, cannon_north_rect = cannon_north.move_clockwise()
+            cannon_south_img, cannon_south_rect = cannon_south.move_clockwise()
+            cannon_west_img, cannon_west_rect = cannon_west.move_clockwise()
+            cannon_north_east_img, cannon_north_east_rect = cannon_north_east.move_clockwise()
 
         if keyboard.key_pressed("SPACE") and shot_cooldown < 0:
             shot_cooldown = cooldown_time
             cannon_north.shot()
-        
-        cannon_north.render_shots()
-            
+            cannon_south.shot()
+            cannon_west.shot()
+            cannon_north_east.shot()        
 
+        cannon_north.render_shots()
+        window.get_screen().blit(cannon_north_img, cannon_north_rect)
+
+        window.get_screen().blit(cannon_south_img, cannon_south_rect)
+        cannon_south.render_shots()    
+
+        cannon_west.render_shots()
+        window.get_screen().blit(cannon_west_img, cannon_west_rect)
+
+        cannon_north_east.render_shots()
+        window.get_screen().blit(cannon_north_east_img, cannon_north_east_rect)
 
         if shot_cooldown > 0:
             shot_cooldown -= window.delta_time()
-
-        window.get_screen().blit(cannon_north_img, cannon_north_rect)
-
 
         # enemy_pirate_1.draw()
         # enemy_pirate_2.draw()
