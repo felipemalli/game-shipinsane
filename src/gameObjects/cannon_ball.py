b547b5a5fde9d82594e59d6afb7d0178621799ce
felipemalli <felipevahia@gmail.com>
2022-11-05 10:50:45 -0300

    fix: ajust cannon_ball initial position for work to all cannons

--- src/gameObjects/cannon_ball.py
+++ src/gameObjects/cannon_ball.py
@@ -7,12 +7,12 @@ class Cannon_ball:
     def __init__(self, speed, cannon_object):
         self.cannon_object = cannon_object
         self.cannon_rect = cannon_object.get_rect()
-        self.cannon_angle = cannon_object.get_angle()
+        self.cannon_absolute_angle = cannon_object.get_absolute_angle()
+        self.cannon_relative_angle = cannon_object.get_relative_angle()
 
         self.cannon_ball_spt = Sprite("../assets/cannon_ball.png")
-        self.cannon_ball_spt.x = self.cannon_rect.centerx - (self.cannon_ball_spt.width / 2) + 1
-        self.cannon_ball_spt.y = self.cannon_rect.centery - (self.cannon_ball_spt.width / 2) + 1
-
+        self.cannon_ball_spt.x = self.cannon_rect.centerx - (self.cannon_ball_spt.width / 2) - (6 * (math.cos(math.radians(90 - self.cannon_relative_angle))) + (10 * (math.cos(math.radians(90 - self.cannon_absolute_angle)))) - 1)
+        self.cannon_ball_spt.y = self.cannon_rect.centery - (self.cannon_ball_spt.width / 2) - (6 * (math.sin(math.radians(90 - self.cannon_relative_angle))) + (10 * (math.sin(math.radians(90 - self.cannon_absolute_angle)))) - 1)
         self.speed = speed                  # speed with delta_time included!
 
     def draw(self):
@@ -28,7 +28,7 @@ class Cannon_ball:
 
     def move_with_angle(self):
         if self.is_out_of_screen(): self.cannon_object.remove_cannon_ball(self)
-        angle = self.cannon_angle
+        angle = self.cannon_absolute_angle
         radAngle = math.radians(90 - angle)
         self.cannon_ball_spt.x -= self.speed * math.cos(radAngle)
         self.cannon_ball_spt.y -= self.speed * math.sin(radAngle)
