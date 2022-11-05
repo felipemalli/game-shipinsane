7e6f8cb8971fa309a38fa9898a26eeb72ee49214
felipemalli <felipevahia@gmail.com>
2022-11-05 13:14:58 -0300

    feat: implement sprite_direction utility

--- src/utils/sprite_direction.py
+++ src/utils/sprite_direction.py
@@ -1,7 +1,10 @@
 from PPlay.sprite import Sprite
 
 
-def sprite_direction(relative_path, png_name, direction):
+def sprite_direction(relative_path, png_name, direction, x , y):
   sprite_path = relative_path + png_name + '_' + direction + '.png'
+  sprite = Sprite(sprite_path)
+  sprite.x = x
+  sprite.y = y
 
-  return Sprite(sprite_path)
+  return sprite
