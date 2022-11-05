3271c2ee942491e52e9af3248c3a498ddb653e2f
felipemalli <felipevahia@gmail.com>
2022-11-05 19:16:00 -0300

    fix: undo elif

--- src/gameObjects/player.py
+++ src/gameObjects/player.py
@@ -41,11 +41,11 @@ class Player:
     def movement(self):
         if keyboard.key_pressed("W"):
             self.sprite.y -= 0.5
-        elif keyboard.key_pressed("A"):
+        if keyboard.key_pressed("A"):
             self.sprite.x -= 0.5
-        elif keyboard.key_pressed("S"):           
+        if keyboard.key_pressed("S"):           
             self.sprite.y += 0.5
-        elif keyboard.key_pressed("D"):           
+        if keyboard.key_pressed("D"):           
             self.sprite.x += 0.5
 
         self.player_direction()
