48a138f1acf6656d10e366360fa849e2cb6cb277
felipemalli <felipevahia@gmail.com>
2022-11-06 20:08:50 -0300

    feat: implement island border restriction for player

--- src/gameObjects/player.py
+++ src/gameObjects/player.py
@@ -3,19 +3,32 @@ import sys
 
 import pygame
 from PPlay.keyboard import Keyboard
+from PPlay.sprite import Sprite
 
 keyboard = Keyboard()
 
 sys.path.insert(0, os.path.abspath("../../")) # src/
-from src.pages.game_parts.window_game import HEIGHT, WIDTH, island, window
+from src.pages.game_parts.window_game import HEIGHT, WIDTH, window
 from src.utils.sprite_direction import sprite_direction
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, island):
+        self.island = island
         self.sprite = sprite_direction('../assets/', 'player', 'S', island.x + island.width / 2 - 35, island.y + island.height / 2 - 25)
         self.cannon_ammo = 3
         self.life = 100
+
+        self.key_W = 'free'
+        self.key_A = 'free'
+        self.key_S = 'free'
+        self.key_D = 'free'
+
+    def set_sprite_x(self, x):
+        self.sprite.x = x
+
+    def set_sprite_y(self, y):
+        self.sprite.y = y
 
     def reduce_cannon_ammo(self):
         if self.cannon_ammo > 0:
@@ -29,6 +42,9 @@ class Player:
 
     def get_sprite(self):
         return self.sprite
+
+    def get_x(self):
+        return self.sprite.x
 
     def player_direction(self):
         if keyboard.key_pressed("W") and keyboard.key_pressed("D"):
@@ -48,17 +64,62 @@ class Player:
         elif keyboard.key_pressed("D"):
             self.sprite = sprite_direction('../assets/', 'player', 'E', self.sprite.x, self.sprite.y)
 
-    def movement(self):
-        if keyboard.key_pressed("W"):
-            self.sprite.y -= 0.5
-        if keyboard.key_pressed("A"):
-            self.sprite.x -= 0.5
-        if keyboard.key_pressed("S"):           
-            self.sprite.y += 0.5
-        if keyboard.key_pressed("D"):           
-            self.sprite.x += 0.5
+    def is_inside_island(self):
+        if pygame.sprite.collide_mask(self.sprite, self.island):
+            return True
 
+    def block_if_outside_island(self, fake_sprite, key):
+        if not pygame.sprite.collide_mask(fake_sprite.get_sprite(), self.island):
+            key_attribute = 'key_' + key
+            setattr(self, key_attribute, 'blocked')
+
+    def check_if_outside_island(self):
+        fake_x = self.sprite.x
+        fake_y = self.sprite.y
+        fake_sprite = Player(self.island)
+
+        fake_sprite.get_sprite().y = fake_y
+        fake_sprite.get_sprite().x = fake_x + 25
+        fake_sprite.get_sprite().draw()
+        self.block_if_outside_island(fake_sprite, 'D')
+
+        fake_sprite.get_sprite().x = fake_x - 25
+        fake_sprite.get_sprite().draw()
+        self.block_if_outside_island(fake_sprite, 'A')
+
+        fake_sprite.get_sprite().x = fake_x
+        fake_sprite.get_sprite().y = fake_y - 5
+        fake_sprite.get_sprite().draw()
+        self.block_if_outside_island(fake_sprite, 'W')
+
+        fake_sprite.get_sprite().x = fake_x
+        fake_sprite.get_sprite().y = fake_y + 38
+        fake_sprite.get_sprite().draw()
+        self.block_if_outside_island(fake_sprite, 'S')
+
+
+    def check_if_inside_island(self):
+        if self.key_W == 'blocked' and self.is_inside_island(): self.key_W = 'free'
+        if self.key_A == 'blocked' and self.is_inside_island(): self.key_A = 'free'
+        if self.key_S == 'blocked' and self.is_inside_island(): self.key_S = 'free'
+        if self.key_D == 'blocked' and self.is_inside_island(): self.key_D = 'free'
+        
+    def movement(self):
+        if keyboard.key_pressed("W") and self.key_W == 'free':
+            self.sprite.y -= 0.7
+            self.check_if_outside_island()
+        if keyboard.key_pressed("A") and self.key_A == 'free':
+            self.sprite.x -= 0.7
+            self.check_if_outside_island()
+        if keyboard.key_pressed("S") and self.key_S == 'free':
+            self.sprite.y += 0.7
+            self.check_if_outside_island()
+        if keyboard.key_pressed("D") and self.key_D == 'free':
+            self.sprite.x += 0.7
+            self.check_if_outside_island()
+        
         self.player_direction()
+        self.check_if_inside_island()
 
     def reload_chest(self):
         font = pygame.font.SysFont("Arial", 30, False, False)
