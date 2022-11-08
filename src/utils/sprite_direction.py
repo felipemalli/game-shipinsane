e68b098ae93d617ce154bee4ecfeb1794feff8ac
felipemalli <felipevahia@gmail.com>
2022-11-08 18:44:48 -0300

    feat: change sprite_direction to not require x and y

--- src/utils/sprite_direction.py
+++ src/utils/sprite_direction.py
@@ -1,10 +1,11 @@
 from PPlay.sprite import Sprite
 
 
-def sprite_direction(relative_path, png_name, direction, x , y):
+def sprite_direction(relative_path, png_name, direction, x = None, y = None):
   sprite_path = relative_path + png_name + '_' + direction + '.png'
   sprite = Sprite(sprite_path)
-  sprite.x = x
-  sprite.y = y
+  if x and y:
+    sprite.x = x
+    sprite.y = y
 
   return sprite
