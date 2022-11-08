79e010b1e43a51fae53723c800ef0a228bd84344
felipemalli <felipevahia@gmail.com>
2022-11-08 18:45:39 -0300

    feat: add create and render ship method for fleet of ships class

--- src/gameObjects/fleet_of_ships.py
+++ src/gameObjects/fleet_of_ships.py
@@ -1,30 +1,41 @@
 import random
 
+from .ship import Ship
+
 
 class Fleet_of_ships:
-    def __init__(self, max_count, average_spawn_speed):
+    def __init__(self, island, max_count = 10, average_spawn_speed = 400, enemy_ship_life = 100, enemy_ship_speed = 25):
+        self.island = island
         self.enemy_ships = []
         self.max_count = max_count
+        self.enemy_ship_life = enemy_ship_life
+        self.enemy_ship_speed = enemy_ship_speed
         self.average_spawn_speed = average_spawn_speed
 
         self.spawn_speed = self.average_spawn_speed
-        self.next_ship_initial_timer = 100
+        self.next_ship_initial_timer = 1000
         self.next_ship_timer = self.next_ship_initial_timer
     
     def generate_enemy_ships(self, delta_time):
         if len(self.enemy_ships) < self.max_count and self.next_ship_timer > 0:
-            self.next_ship_timer -= self.random_spawn_speed * delta_time
+            self.next_ship_timer -= self.spawn_speed * delta_time
         
         if self.next_ship_timer <= 0:
-            self.spawn_speed = self.generate_random_num_around(self.enemy_ship_spawn_speed, 1/3)
+            self.spawn_speed = self.generate_random_num_around(self.average_spawn_speed, 30)
             self.next_ship_timer = self.next_ship_initial_timer
             self.create_enemy_ship()
-  
-    def generate_random_num_around(self, num, proximity):
-        min_num = num / proximity
-        max_num = num * proximity
-        return random.randint(min_num, max_num)
 
     def create_enemy_ship(self):
-        enemy_ship = Ship(self)
+        enemy_ship = Ship(self.island, self.enemy_ship_life, self.enemy_ship_speed)
         self.enemy_ships.append(enemy_ship)
+
+    def render_ships(self, delta_time):
+        print(self.enemy_ships)
+        for enemy_ship in self.enemy_ships:
+            enemy_ship.draw()
+            enemy_ship.move(delta_time)
+
+    def generate_random_num_around(self, num, proximity):
+        min_num = num - proximity
+        max_num = num + proximity
+        return random.randint(min_num, max_num)
