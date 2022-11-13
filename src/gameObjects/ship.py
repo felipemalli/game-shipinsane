ae9e74f9418a2db2d3a49878f86a2d3e30019c1e
felipemalli <felipevahia@gmail.com>
2022-11-13 17:22:15 -0300

    feat: add random speed for side directions of ships

--- src/gameObjects/ship.py
+++ src/gameObjects/ship.py
@@ -6,6 +6,7 @@ import pygame
 
 sys.path.insert(0, os.path.abspath("../../")) # src/
 from src.pages.game_parts.window_game import HEIGHT, WIDTH, window
+from src.utils.ship_moviment import get_around_string_list
 from src.utils.sprite_utilities import Sprite_utils
 
 
@@ -20,9 +21,16 @@ class Ship:
         self.sprite = Sprite_utils.sprite_direction('../assets/', 'enemy_ship', self.direction)
         self.set_position_out_of_screen()
 
+
+        self.all_directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+        self.possible_directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
         self.average_change_direction_speed = 100
         self.change_direction_initial_timer = 20
         self.change_direction_timer = self.change_direction_initial_timer
+
+        self.change_random_speed = 5
+        self.random_speed_NS = 0
+        self.random_speed_EW = 0
 
     def reduce_life(self):
         self.life -= 1
@@ -45,11 +53,11 @@ class Ship:
 
     #   ----------- Initial position with middle included ------------
     def set_position_out_of_screen(self):
-        if self.direction == 'S': # coming from up
+        if self.direction == 'S': # coming from above
             self.sprite.y = - self.sprite.height
             self.sprite.x = random.randint(0, WIDTH - self.sprite.width)
             return
-        if self.direction == 'N': # coming from down
+        if self.direction == 'N': # coming from below
             self.sprite.y = HEIGHT
             self.sprite.x = random.randint(0, WIDTH - self.sprite.width)
             return
@@ -82,28 +90,41 @@ class Ship:
     #         self.sprite.x = WIDTH
     #         return
 
+    # def random_speed_NS_possibility(self):
+    #     if (close_to_north() and (self.direction == 'E' or self.direction == 'W')):
+    #         return [3]
+    #     if (close_to_south() and (self.direction == 'E' or self.direction == 'W')):
+    #         return [-3]
+    #     return [-3, 3]
+    
+    # def random_speed_EW_possibility(self):
+    #     if (close_to_east() and (self.direction == 'N' or self.direction == 'S')):
+    #         return [-3]
+    #     if (close_to_west() and (self.direction == 'N' or self.direction == 'S')):
+    #         return [3]
+    #     return [-3, 3]
+
+    def random_side_speed(self, delta_time):
+        self.change_random_speed += (2* delta_time)
+        if self.change_random_speed >= (10 + random.randint(-3, 3)):
+            self.random_speed_NS = random.choice([3, -3]) * delta_time
+            self.random_speed_EW = random.choice([3, -3]) * delta_time
+            self.change_random_speed = 0
+
     def move_to_a_direction(self, direction, delta_time):
+        self.random_side_speed(delta_time)
         if 'N' in direction:
             self.sprite.y -= self.speed * delta_time
+            self.sprite.x += self.random_speed_EW
         if 'S' in direction:
             self.sprite.y += self.speed * delta_time
+            self.sprite.x += self.random_speed_EW
         if 'E' in direction:
             self.sprite.x += self.speed * delta_time
+            self.sprite.y += self.random_speed_NS
         if 'W' in direction:
             self.sprite.x -= self.speed * delta_time
-
-    # def collide_mask_rect(self, left, right):
-    #     xoffset = right.rect[0] - left.x
-    #     yoffset = right.rect[1] - left.y
-    #     try:
-    #         leftmask = left.mask
-    #     except AttributeError:
-    #         leftmask = pygame.mask.Mask(left.size, True)
-    #     try:
-    #         rightmask = right.mask
-    #     except AttributeError:
-    #         rightmask = pygame.mask.from_surface(right.image)
-    #     return leftmask.overlap(rightmask, (xoffset, yoffset))
+            self.sprite.y += self.random_speed_NS
 
     def collide_with_island(self):
         # if pygame.sprite.collide_mask(self.sprite, self.island):
@@ -111,12 +132,41 @@ class Ship:
             return True
         return False
 
+    # def is_close_to_above(self):
+    #     print()
+        # if self.hitbox.y < 50:
+        # if self.direction == 'N':
+        #     self.direction = random.choice('NE', 'NW')
+
+    def remove_directions(self, letters):
+        for letter in letters:
+            self.possible_directions.remove(letter)
+
+    def change_direction(self):
+        directions = get_around_string_list(self.possible_directions, self.direction)
+        self.direction = random.choice(directions)
+    
+    def is_close_to_below(self):
+        if self.hitbox.y > HEIGHT - 200 and self.direction == 'S':
+            self.change_direction()
+            return self.remove_directions('S')
+        # print(self.hitbox.y)
+        if self.hitbox.y > HEIGHT - 150 and (self.direction == 'SW' or self.direction == 'SE'):
+            self.remove_directions(['SE', 'S', 'SW'])
+            return self.change_direction()
+        self.possible_directions = self.all_directions
+
+
     # def about_to_leave_map(self):
+    #     self.all_directions
+
+
     #     if self.sprite.x +=
 
     def move(self, delta_time):
         if not self.collide_with_island():
             # if self.about_to_leave_map():
+            self.is_close_to_below()
                 
 
             # if self.change_direction_timer > 0:
--- src/utils/ship_moviment.py
+++ src/utils/ship_moviment.py
@@ -1,11 +1,4 @@
-
-side_options = ['nw', 'n', 'ne', 'e', 'se', 's', 'sw', 'w']
-
-print(side_options)
-
-# side_options.index(primary_side) 
-
-def get_around_index_list(iterated_list, center_index, range_size):
+def get_around_index_list_by_range(iterated_list, center_index, range_size):
     around_list = list()
     
     for i in range(1, range_size + 1):
@@ -21,16 +14,15 @@ def get_around_index_list(iterated_list, center_index, range_size):
 
     return around_list
 
-around_list = get_around_index_list(side_options, 6, 2)
-print(around_list)
 
-# if not side_options['sw']:
-#     print(side_options['sw'])
-# if test.contains("_ne"):
+def get_around_string_list(string_list, string):
+    around_list = []
+    
+    direction_i = string_list.index(string)
+    
+    if (direction_i) == len(string_list) - 1: around_list.append(string_list[0])
+    else: around_list.append(string_list[direction_i + 1])
+    if (direction_i) < 0: around_list.append(string_list[len(direction_i) - 1])
+    else: around_list.append(string_list[direction_i - 1])
 
-# 'n'
-
-# nw []
-
-# def define_cannon_rotation(primary_side, max_rotation):
-#     side[primary_side] = 
+    return around_list
